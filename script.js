a40d3188781551ecc39211c47788e285015a7f47
tarunkhur <tarunk.0810@gmail.com>
2023-02-27 23:16:48 +0530

    removing consoles

--- script.js
+++ script.js
@@ -19,15 +19,12 @@ let password = "";
 let passwordLength = 10;
 let checkCount = 0;
 handleSlider();
-//ste strength circle color to grey
 setIndicator("#ccc");
 
 
-//set passwordLength
 function handleSlider() {
     inputSlider.value = passwordLength;
     lengthDisplay.innerText = passwordLength;
-    //or kuch bhi karna chahiye ? - HW
     const min = inputSlider.min;
     const max = inputSlider.max;
     inputSlider.style.backgroundSize = ( (passwordLength - min)*100/(max - min)) + "% 100%"
@@ -90,7 +87,6 @@ async function copyContent() {
     catch(e) {
         copyMsg.innerText = "Failed";
     }
-    //to make copy wala span visible
     copyMsg.classList.add("active");
 
     setTimeout( () => {
@@ -100,11 +96,8 @@ async function copyContent() {
 }
 
 function shufflePassword(array) {
-    //Fisher Yates Method
     for (let i = array.length - 1; i > 0; i--) {
-        //random J, find out using random function
         const j = Math.floor(Math.random() * (i + 1));
-        //swap number at i index and j index
         const temp = array[i];
         array[i] = array[j];
         array[j] = temp;
@@ -121,7 +114,6 @@ function handleCheckBoxChange() {
             checkCount++;
     });
 
-    //special condition
     if(passwordLength < checkCount ) {
         passwordLength = checkCount;
         handleSlider();
@@ -145,7 +137,6 @@ copyBtn.addEventListener('click', () => {
 })
 
 generateBtn.addEventListener('click', () => {
-    //none of the checkbox are selected
 
     if(checkCount == 0) 
         return;
@@ -155,28 +146,8 @@ generateBtn.addEventListener('click', () => {
         handleSlider();
     }
 
-    // let's start the jouney to find new password
     console.log("Starting the Journey");
-    //remove old password
     password = "";
-
-    //let's put the stuff mentioned by checkboxes
-
-    // if(uppercaseCheck.checked) {
-    //     password += generateUpperCase();
-    // }
-
-    // if(lowercaseCheck.checked) {
-    //     password += generateLowerCase();
-    // }
-
-    // if(numbersCheck.checked) {
-    //     password += generateRandomNumber();
-    // }
-
-    // if(symbolsCheck.checked) {
-    //     password += generateSymbol();
-    // }
 
     let funcArr = [];
 
@@ -192,25 +163,20 @@ generateBtn.addEventListener('click', () => {
     if(symbolsCheck.checked)
         funcArr.push(generateSymbol);
 
-    //compulsory addition
     for(let i=0; i<funcArr.length; i++) {
         password += funcArr[i]();
     }
     console.log("COmpulsory adddition done");
 
-    //remaining adddition
     for(let i=0; i<passwordLength-funcArr.length; i++) {
         let randIndex = getRndInteger(0 , funcArr.length);
         console.log("randIndex" + randIndex);
         password += funcArr[randIndex]();
     }
     console.log("Remaining adddition done");
-    //shuffle the password
     password = shufflePassword(Array.from(password));
     console.log("Shuffling done");
-    //show in UI
     passwordDisplay.value = password;
     console.log("UI adddition done");
-    //calculate strength
     calcStrength();
 });
